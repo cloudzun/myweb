422022929eba3c4448617d5d4ab5fa86f34eec6f
CloudZun <info@cloudzun.com>
2023-05-08 14:07:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
 
-def get_location():
+def get_location(ip_address=None):
     try:
-        response = requests.get('http://ip-api.com/json/')
+        url = f'http://ip-api.com/json/{ip_address}' if ip_address else 'http://ip-api.com/json/'
+        response = requests.get(url)
         location_data = response.json()
         if location_data['status'] == 'success':
             return {
@@ -22,8 +23,9 @@ def get_location():
 @app.route('/')
 def get_hostname():
     hostname = os.environ.get('HOST_HOSTNAME', '未知')
-    location = get_location()
+    server_location = get_location()
     visitor_ip = request.remote_addr
+    visitor_location = get_location(visitor_ip)
     return render_template_string('''
     <!DOCTYPE html>
     <html>
@@ -36,20 +38,34 @@ def get_hostname():
             </style>
         </head>
         <body>
-            <h3>当前服务器的计算机名是: {{ hostname }}</h3>
-            <h3>服务器位置: {{ location.location }}</h3>
-            <h3>访问者地址: {{ visitor_ip }}</h3>
+            <h3>1. 当前宿主机的计算机名是: {{ hostname }}</h3>
+            <h3>2. 服务器位置: {{ server_location.location }}</h3>
+            <h3>3. 访问者地址: {{ visitor_ip }} ({{ visitor_location.location }})</h3>
             <div id="map"></div>
             <script>
-                var map = L.map('map').setView([{{ location.latitude }}, {{ location.longitude }}], 13);
+                var map = L.map('map').fitBounds([
+                    [{{ server_location.latitude }}, {{ server_location.longitude }}],
+                    [{{ visitor_location.latitude }}, {{ visitor_location.longitude }}]
+                ]);
                 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
-                L.marker([{{ location.latitude }}, {{ location.longitude }}]).addTo(map);
+
+                var serverMarker = L.marker([{{ server_location.latitude }}, {{ server_location.longitude }}]).addTo(map);
+                serverMarker.bindPopup("服务器位置: {{ server_location.location }}").openPopup();
+
+                var visitorMarker = L.marker([{{ visitor_location.latitude }}, {{ visitor_location.longitude }}]).addTo(map);
+                visitorMarker.bindPopup("访问者位置: {{ visitor_location.location }}");
+
+                var latlngs = [
+                    [{{ server_location.latitude }}, {{ server_location.longitude }}],
+                    [{{ visitor_location.latitude }}, {{ visitor_location.longitude }}]
+                ];
+                var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
             </script>
         </body>
     </html>
-    ''', hostname=hostname, location=location, visitor_ip=visitor_ip)
+    ''', hostname=hostname, server_location=server_location, visitor_ip=visitor_ip, visitor_location=visitor_location)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
